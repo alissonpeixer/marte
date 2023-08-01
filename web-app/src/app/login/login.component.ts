4a68330fae13644ea023bb1f81d7590ab353fd75
alissonpeixer <alissonpeixer4@gmail.com>
2023-08-01 01:05:19 +0000

    feat: login page

--- web-app/src/app/login/login.component.ts
+++ web-app/src/app/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PoDialogService } from '@po-ui/ng-components';
+import { PoPageLogin } from '@po-ui/ng-templates';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  exceededAttempts: number = 0;
+
+  constructor(
+    private poDialog: PoDialogService
+  ){
+  }
+
+  ngOnInit() {
+
+  }
+
+  loginSubmit(formData: PoPageLogin) {
+    if (this.exceededAttempts <= 0) {
+      this.poDialog.alert({
+        title: 'Authenticate',
+        message: JSON.stringify(formData)
+      });
+    }
+  }
 
 }
