17c408348ab79f2dbb9777dd72c1db1ce0989ab5
alissonpeixer <alissonpeixer4@gmail.com>
2023-08-01 00:29:20 +0000

    feat: rediret to login page

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, data: { permissao: [] } },
 ];
 
